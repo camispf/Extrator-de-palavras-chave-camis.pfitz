af10c0f37e694684bb55129000f72ac5a65d2510
Camispfitz <camile.pfitz@escola.pr.gov.br>
2025-07-03 22:32:10 +0000

    projeto final

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+import { PALAVRAS_RUINS } from "./palavrasRuins.js";
 const botaoMostraPalavras = document.querySelector("#botao-palavrachave");
 
 botaoMostraPalavras.addEventListener("click", mostraPalavrasChave);
@@ -12,11 +13,14 @@ function mostraPalavrasChave() {
 
 function processaTexto(texto) {
   let palavras = texto.split(/\P{L}+/u);
+for (let i in palavras) {
+  palavras[i] = palavras[i].toLowerCase();
+}
+
+  palavras = tiraPalavrasRuins(palavras);
 
   const frequencias = contaFrequencias(palavras);
-
   let ordenadas = Object.keys(frequencias).sort(ordenaPalavra);
-
   function ordenaPalavra(p1, p2) {
     return frequencias[p2] - frequencias[p1];
   }
@@ -26,10 +30,8 @@ function processaTexto(texto) {
 
 function contaFrequencias(palavras) {
   let frequencias = {};
-
   for (let i of palavras) {
     frequencias[i] = 0;
-
     for (let j of palavras) {
       if (i == j) {
         frequencias[i]++;
@@ -38,4 +40,16 @@ function contaFrequencias(palavras) {
   }
 
   return frequencias;
+}
+
+function tiraPalavrasRuins(palavras) {
+
+  const palavrasBoas = [];
+  for (let palavra of palavras) {
+    if (!PALAVRAS_RUINS.has(palavra) && palavra.length > 2) {
+      palavrasBoas.push(palavra);
+    }
+  }
+
+  return palavrasBoas;
 }
